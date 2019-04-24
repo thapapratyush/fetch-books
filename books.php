6b3d3cb57e2008c703b8059d18db932d026d86a5
thapapratyush <thapa.pratyush10@gmail.com>
2019-04-23 20:46:32 -0400

    Add books.js

--- books.php
+++ books.php
@@ -1,7 +1,3 @@
 <?php
-    $a = fopen("books.json","r")or die("Unable to open file!");
-    $content = fread($a,filesize("books.json"));
-    $assocarray = json_decode($content, true);
-    fclose($a);
-    print_r($assocarray, true);
+    $scho "<script type='text/javascript' src=book.js'></script>"
 ?>
