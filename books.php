6478ec7e661c37192d05542d1834ba0eb2352235
thapapratyush <thapa.pratyush10@gmail.com>
2019-04-24 23:13:36 -0400

    Add prototype.js

--- books.php
+++ books.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <script type="text/javascript" src="books.js" ></script>
+        <script type="text/javascript" src="prototype.js" ></script>
     </head>
     <body>
     <div id="categories"></div>
