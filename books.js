0e384fd19fe1847c5943c2b51ae7ca56011f3ffb
thapapratyush <thapa.pratyush10@gmail.com>
2019-04-24 23:43:42 -0400

    Add ability to fetch categories from js and populate a list

--- books.js
+++ books.js
@@ -10,7 +10,17 @@ window.onload = function(e){
 function fetchCategories(response) {
     if (response.responseXML) {
         responseXML = response.responseXML;
-        console.log(responseXML);  
+        var categories = responseXML.getElementsByTagName("categories")[0];
+        var firstchild = categories.firstChild;
+
+        var list_of_categories = []
+        while (firstchild != null) {
+            var name = firstchild.firstChild.firstChild.nodeValue;
+            var id = firstchild.lastChild.firstChild.nodeValue;
+            list_of_categories.push([name, id]);
+            //move on to the next sibling to find other categories
+            firstchild = firstchild.nextSibling;
+        }  
     } else {
         console.log("Could not parse the response");
     }
