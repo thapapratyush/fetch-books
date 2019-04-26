dc3c9fdcc205a9ab41d218ab34775bf329334e9f
Pratyush Thapa <thapa.pratyush10@gmail.com>
2019-04-26 16:12:18 -0400

    Added Dump of the SQL

--- db.sql
+++ db.sql
@@ -1,77 +1,158 @@
-CREATE DATABASE IF NOT EXISTS listofbooks;
-USE listofbooks;
+-- MySQL dump 10.16  Distrib 10.1.37-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: listofbooks
+-- ------------------------------------------------------
+-- Server version	10.1.37-MariaDB-0+deb9u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `listofbooks`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `listofbooks` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `listofbooks`;
 
 CREATE USER 'prat'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON listofbooks.* to 'prat'@'localhost';
 
-create table title (
-    title_id int not null,
-    title_name varchar(255) not null,
-    category_id int,
-    author_id int,
-    primary key (title_id)
-);
+--
+-- Table structure for table `author`
+--
 
-create table year (
-    year int(4) not null,
-    title_id int
-);
+DROP TABLE IF EXISTS `author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `author` (
+  `author_id` int(11) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  PRIMARY KEY (`author_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-create table price (
-    price decimal(18, 4) not null,
-    title_id int
-);
+--
+-- Dumping data for table `author`
+--
 
-create table category (
-    category_id int not null,
-    category varchar(32) not null,
-    primary key (category_id)
-);
+LOCK TABLES `author` WRITE;
+/*!40000 ALTER TABLE `author` DISABLE KEYS */;
+INSERT INTO `author` VALUES (1,'William Shakespeare'),(2,'Emily Dickinson'),(3,'H. P. Lovecraft'),(4,'Arthur Conan Doyle'),(5,'Leo Tolstoy'),(6,'Edgar Allan Poe'),(7,'Robert Ervin Howard'),(8,'Rabindranath Tagore');
+/*!40000 ALTER TABLE `author` ENABLE KEYS */;
+UNLOCK TABLES;
 
-create table author (
-    author_id int not null,
-    author varchar(255) not null,
-    primary key (author_id)
-);
+--
+-- Table structure for table `category`
+--
 
-INSERT INTO category VALUES(1,"RomCom");
-INSERT INTO category VALUES(2,"Comedy");
-INSERT INTO category VALUES(3,"Thriller");
-INSERT INTO category VALUES(4,"Bollywood");
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL,
+  `category` varchar(32) NOT NULL,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO author VALUES(1,"William Shakespeare");
-INSERT INTO author VALUES(2,"Emily Dickinson");
-INSERT INTO author VALUES(3,"H. P. Lovecraft");
-INSERT INTO author VALUES(4,"Arthur Conan Doyle");
-INSERT INTO author VALUES(5,"Leo Tolstoy");
-INSERT INTO author VALUES(6,"Edgar Allan Poe");
-INSERT INTO author VALUES(7,"Robert Ervin Howard");
-INSERT INTO author VALUES(8,"Rabindranath Tagore");
+--
+-- Dumping data for table `category`
+--
 
-INSERT INTO title VALUES(1,"The rainy day",1,1);
-INSERT INTO title VALUES(2,"Once upon a time",2,2);
-INSERT INTO title VALUES(3,"Mission Impossible",3,3);
-INSERT INTO title VALUES(4,"The flower girl",4,4);
-INSERT INTO title VALUES(5,"RainMan",1,5);
-INSERT INTO title VALUES(6,"Home Alone",2,6);
-INSERT INTO title VALUES(7,"The Fault in our stars",4,7);
-INSERT INTO title VALUES(8,"Antione Bruins",3,8);
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'RomCom'),(2,'Comedy'),(3,'Thriller'),(4,'Bollywood');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
 
-INSERT INTO year VALUES (2002,1);
-INSERT INTO year VALUES (1785,2);
-INSERT INTO year VALUES (1895,3);
-INSERT INTO year VALUES (1996,4);
-INSERT INTO year VALUES (1876,5);
-INSERT INTO year VALUES (2012,6);
-INSERT INTO year VALUES (1987,7);
-INSERT INTO year VALUES (2018,8);
+--
+-- Table structure for table `price`
+--
 
+DROP TABLE IF EXISTS `price`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `price` (
+  `price` decimal(18,4) NOT NULL,
+  `title_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-INSERT INTO price VALUES (278,1);
-INSERT INTO price VALUES (134,2);
-INSERT INTO price VALUES (23,3);
-INSERT INTO price VALUES (18,4);
-INSERT INTO price VALUES (202,5);
-INSERT INTO price VALUES (132,6);
-INSERT INTO price VALUES (67,7);
-INSERT INTO price VALUES (45,8);
+--
+-- Dumping data for table `price`
+--
+
+LOCK TABLES `price` WRITE;
+/*!40000 ALTER TABLE `price` DISABLE KEYS */;
+INSERT INTO `price` VALUES (278.0000,1),(134.0000,2),(23.0000,3),(18.0000,4),(202.0000,5),(132.0000,6),(67.0000,7),(45.0000,8);
+/*!40000 ALTER TABLE `price` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `title`
+--
+
+DROP TABLE IF EXISTS `title`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `title` (
+  `title_id` int(11) NOT NULL,
+  `title_name` varchar(255) NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `author_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`title_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `title`
+--
+
+LOCK TABLES `title` WRITE;
+/*!40000 ALTER TABLE `title` DISABLE KEYS */;
+INSERT INTO `title` VALUES (1,'The rainy day',1,1),(2,'Once upon a time',2,2),(3,'Mission Impossible',3,3),(4,'The flower girl',4,4),(5,'RainMan',1,5),(6,'Home Alone',2,6),(7,'The Fault in our stars',4,7),(8,'Antione Bruins',3,8);
+/*!40000 ALTER TABLE `title` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `year`
+--
+
+DROP TABLE IF EXISTS `year`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `year` (
+  `year` int(4) NOT NULL,
+  `title_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `year`
+--
+
+LOCK TABLES `year` WRITE;
+/*!40000 ALTER TABLE `year` DISABLE KEYS */;
+INSERT INTO `year` VALUES (2002,1),(1785,2),(1895,3),(1996,4),(1876,5),(2012,6),(1987,7),(2018,8);
+/*!40000 ALTER TABLE `year` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-04-26 17:59:57
